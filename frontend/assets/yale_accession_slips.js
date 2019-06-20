4e0385b5b24eae642783345672886b4b6d4e6aeb
Payten Giles <payten.giles@gmail.com>
2019-06-20 15:46:45 +1000

    Refactor the slip report as a FOP PDF

--- frontend/assets/yale_accession_slips.js
+++ frontend/assets/yale_accession_slips.js
@@ -10,36 +10,14 @@ NonPrintSlip.prototype.setupLink = function() {
     var self = this;
 
     var $li = $('<li>');
-    var $a = $('<a>').attr('href', 'javascript:void(0)').attr('id', self.linkId).html('Print accession record');
+    var $a = $('<a>')
+                .attr('href', APP_PATH + 'plugins/yale-accession-slips/non-print-slip/'+ ACCESSION_ID)
+                .attr('target', '_blank')
+                .html('Print accession record');
 
     $li.append($a);
     $('#other-dropdown > .dropdown-menu').append($li);
-
-    $a.on('click', function() {
-        self.print();
-    });
 };
-
-NonPrintSlip.prototype.print = function() {
-    $('#'+this.name).remove();
-
-    var $iframe = $('<iframe id="' + this.name + '" name="' + this.name + '"></iframe>');
-
-    var path = APP_PATH + 'plugins/yale-accession-slips/non-print-slip/' + ACCESSION_ID;
-
-    $.get(path, function(html) {
-        if ("srcdoc" in document.createElement("iframe")) {
-            $iframe.attr('srcdoc', html);
-        } else {
-            $iframe.attr('src', 'data:text/html;charset=utf-8,' + encodeURI(html));
-        }
-
-        $iframe.addClass('hide');
-
-        $(document.body).append($iframe);
-    });
-};
-
 
 $(document).ready(function() {
     new NonPrintSlip();
--- frontend/controllers/yale_accession_slips_controller.rb
+++ frontend/controllers/yale_accession_slips_controller.rb
@@ -4,17 +4,61 @@ class YaleAccessionSlipsController < ApplicationController
 
   layout 'yale_accession_slips'
 
+  java_import Java::org::apache::fop::apps::FopFactory
+  java_import Java::org::apache::fop::apps::Fop
+  java_import Java::org::apache::fop::apps::MimeConstants
+  import javax.xml.transform.stream.StreamSource
+  import javax.xml.transform.TransformerFactory
+  import javax.xml.transform.sax.SAXResult
+
   def non_print_slip
     to_resolve = find_opts.fetch("resolve[]")
     to_resolve << 'payment_summary::payments::authorizer'
-
     @accession = Accession.find(params[:id], {
       "resolve[]" => to_resolve,
     })
 
-    @print = params[:print] != '0'
-    render template: 'accessions/non_print_slip'
-  end
+    report_fo = render_to_string(:partial => 'accessions/non_print_slip.fo.erb').strip
 
+    fo_file = ASUtils.tempfile("non_print_slip_fo")
+    fo_file.write(report_fo)
+    fo_file.close
+
+    output_pdf_file = java.io.File.createTempFile("non_print_slip", "pdf")
+    output_stream = java.io.FileOutputStream.new(output_pdf_file)
+
+    begin
+      input_stream = java.io.FileInputStream.new(fo_file.path)
+
+      fopfac = FopFactory.newInstance
+      fopfac.setBaseURL( File.join(ASUtils.find_base_directory, 'stylesheets') )
+      fop = fopfac.newFop(MimeConstants::MIME_PDF, output_stream)
+      transformer = TransformerFactory.newInstance.newTransformer()
+      res = SAXResult.new(fop.getDefaultHandler)
+      transformer.transform(StreamSource.new(input_stream), res)
+    ensure
+      output_stream.close
+    end
+
+    filename = "accession_#{params[:id]}_summary.pdf"
+
+    respond_to do |format|
+      format.all do
+        fh = File.open(output_pdf_file.path, "r")
+        self.headers["Content-type"] = "application/pdf"
+        self.headers["Content-disposition"] = "attachment; filename=\"#{filename}\""
+        self.response_body = Enumerator.new do |y|
+          begin
+            while chunk = fh.read(4096)
+              y << chunk
+            end
+          ensure
+            fh.close
+            fo_file.unlink
+          end
+        end
+      end
+    end
+  end
 end
 
